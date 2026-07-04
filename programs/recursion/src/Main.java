3882b54c36ba602700f559e4b4de3bad7ae03faa
divyanshu <vadhldivyesh@gmail.com>
2026-07-04 21:00:30 +0530

    add recurive learnings

--- programs/recursion/src/Main.java
+++ programs/recursion/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
+    //print  1 to 5 ok!
+
+    // every recursion have to case
+    // 1 will be the base case: where recursion stops
+    // 2. recursive call: is where recuren sitars with new arguments
+
+    public  static  void print(int start,int end){
+        // base case :
+        if(start>end) return ;
+
+        //recursive call :
+        System.out.println(start);
+        print(start+1,end);
+        // starts printing from 1;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        print(1,5);
     }
 }
